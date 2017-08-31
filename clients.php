b1c46b11d5b1a728ade7c4bb9ed7712da3279909
qusielle <31454380+qusielle@users.noreply.github.com>
2017-08-31 21:04:47 +0300

    Fix slash to backslash in utorrent's downloads path string. Wrong slash causes weird errors and download couldn't start.

--- clients.php
+++ clients.php
@@ -197,7 +197,7 @@ class utorrent {
 		$this->setSetting('dir_active_download_flag', true);
 		foreach($filename as $file){
 			if (!empty($savepath)) {
-				$current_savepath = $savepath_subfolder ? $savepath . '/' . $file['id'] : $savepath;
+				$current_savepath = $savepath_subfolder ? $savepath . '\\' . $file['id'] : $savepath;
 				$this->setSetting('dir_active_download', urlencode($current_savepath));
 			}
 			$this->makeRequest("?action=add-url&s=".urlencode($file['filename']), false);
