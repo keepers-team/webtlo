2db764f44768834abeb974c0996319ba01578994
berkut-174 <berkut_174@mail.ru>
2017-09-20 13:50:49 +0500

    [fix] Сохранение раздач в уникальные подкаталоги
    Вообще, кстати, вот так должно быть...

--- clients.php
+++ clients.php
@@ -197,8 +197,10 @@ class utorrent {
 		$this->setSetting('dir_active_download_flag', true);
 		foreach($filename as $file){
 			if (!empty($savepath)) {
-				$current_savepath = $savepath_subfolder ? $savepath . '\\' . $file['id'] : $savepath;
-				$this->setSetting('dir_active_download', urlencode($current_savepath));
+				if ( $savepath_subfolder ) {
+					$savepath = $savepath . $file['id'] . substr( $savepath, -1 );
+				}
+				$this->setSetting('dir_active_download', urlencode($savepath));
 			}
 			$this->makeRequest("?action=add-url&s=".urlencode($file['filename']), false);
 			usleep( 500000 );
