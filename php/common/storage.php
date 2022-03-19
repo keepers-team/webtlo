b8694e1d83dd8cf83a780d415acbe1314cc2ec71
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-03-19 19:07:36 +0200

    Abstract over hardcoded storage location

---
+++ php/common/storage.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Get normalized path, like realpath() for non-existing path or file
+ *
+ * @param string $path path to be normalized
+ */
+function normalizePath(string $path): string
+{
+    return array_reduce(explode(DIRECTORY_SEPARATOR, $path), function ($left, $right) {
+        if ($left === null) {
+            $left = DIRECTORY_SEPARATOR;
+        }
+        if ($right === "" || $right === ".") {
+            return $left;
+        }
+        if ($right === "..") {
+            return dirname($left);
+        }
+        $pattern = sprintf("/\%s+/", DIRECTORY_SEPARATOR);
+        return preg_replace($pattern, DIRECTORY_SEPARATOR, $left . DIRECTORY_SEPARATOR . $right);
+    });
+}
+
+/**
+ * @return string Storage directory for application
+ */
+function getStorageDir(): string
+{
+    $directory = getenv('WEBTLO_DIR');
+    if ($directory === false) {
+        return normalizePath(dirname(__FILE__) . "/../../data");
+    } else {
+        return $directory;
+    }
+}
