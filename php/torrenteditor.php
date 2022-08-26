5633db664198aebdd501f9c7853a85a2a3b4fbd2
Alexander Shemetov <berkut_174@mail.ru>
2022-08-26 13:40:59 +0500

    TorrentEditor: Drop deprecated code and fix coding style

--- php/torrenteditor.php
+++ php/torrenteditor.php
@@ -27,295 +27,278 @@ class Torrent
     // Private class members
     private $torrent;
     private $info;
-    
+
     // Public error message, $error is set if load() returns false
     public $error;
-    
+
     // Load torrent file data
     // $data - raw torrent file contents
-    public function load( &$data )
+    public function load(&$data)
     {
-        $this->torrent = BEncode::decode( $data );
+        $this->torrent = BEncode::decode($data);
 
-        if ( $this->torrent->get_type() == 'error' )
-        {
+        if ($this->torrent->get_type() == 'error') {
             $this->error = $this->torrent->get_plain();
             return false;
-        }
-        else if ( $this->torrent->get_type() != 'dictionary' )
-        {
+        } else if ($this->torrent->get_type() != 'dictionary') {
             $this->error = 'The file was not a valid torrent file.';
             return false;
         }
-        
+
         $this->info = $this->torrent->get_value('info');
-        if ( !$this->info )
-        {
+        if (!$this->info) {
             $this->error = 'Could not find info dictionary.';
             return false;
         }
-        
+
         return true;
     }
-    
+
     // Get comment
     // return - string
-    public function getComment() {
+    public function getComment()
+    {
         return $this->torrent->get_value('comment') ? $this->torrent->get_value('comment')->get_plain() : null;
     }
-    
+
     // Get creatuion date
     // return - php date
-    public function getCreationDate() {
+    public function getCreationDate()
+    {
         return  $this->torrent->get_value('creation date') ? $this->torrent->get_value('creation date')->get_plain() : null;
     }
-    
+
     // Get created by
     // return - string
-    public function getCreatedBy() {
+    public function getCreatedBy()
+    {
         return $this->torrent->get_value('created by') ? $this->torrent->get_value('created by')->get_plain() : null;
     }
-    
+
     // Get name
     // return - filename (single file torrent)
     //          directory (multi-file torrent)
     // see also - getFiles()
-    public function getName() {
+    public function getName()
+    {
         return $this->info->get_value('name')->get_plain();
     }
-    
+
     // Get piece length
     // return - int
-    public function getPieceLength() {
+    public function getPieceLength()
+    {
         return $this->info->get_value('piece length')->get_plain();
     }
-    
+
     // Get pieces
     // return - raw binary of peice hashes
-    public function getPieces() {
+    public function getPieces()
+    {
         return $this->info->get_value('pieces')->get_plain();
     }
-    
+
     // Get private flag
     // return - -1 public, implicit
     //           0 public, explicit
     //           1 private
-    public function getPrivate() {
-        if ( $this->info->get_value('private') )
-        {
+    public function getPrivate()
+    {
+        if ($this->info->get_value('private')) {
             return $this->info->get_value('private')->get_plain();
         }
         return -1;
     }
-    
+
     // Get a list of files
     // return - array of Torrent_File
-    public function getFiles() {
+    public function getFiles()
+    {
         // Load files
         $filelist = array();
         $length = $this->info->get_value('length');
-        
-        if ( $length )
-        {
+
+        if ($length) {
             $file = new Torrent_File();
             $file->name = $this->info->get_value('name')->get_plain();
             $file->length =  $this->info->get_value('length')->get_plain();
-            array_push( $filelist, $file );
-        }
-        else if ( $this->info->get_value('files') )
-        {
+            array_push($filelist, $file);
+        } else if ($this->info->get_value('files')) {
             $files = $this->info->get_value('files')->get_plain();
-            while ( list( $key, $value ) = each( $files ) )
-            {
+            foreach ($files as $key => $value) {
                 $file = new Torrent_File();
 
                 $path = $value->get_value('path')->get_plain();
-                while ( list( $key, $value2 ) = each( $path ) )
-                {
+                foreach ($path as $key => $value2) {
                     $file->name .= "/" . $value2->get_plain();
                 }
-                $file->name = ltrim( $file->name, '/' );
+                $file->name = ltrim($file->name, '/');
                 $file->length =  $value->get_value('length')->get_plain();
 
-                array_push( $filelist, $file );
+                array_push($filelist, $file);
             }
         }
-        
+
         return $filelist;
     }
-    
+
     // Get a list of trackers
     // return - array of strings
-    public function getTrackers() {
+    public function getTrackers()
+    {
         // Load tracker list
         $trackerlist = array();
-        
-        if ( $this->torrent->get_value('announce-list') )
-        {
+
+        if ($this->torrent->get_value('announce-list')) {
             $trackers = $this->torrent->get_value('announce-list')->get_plain();
-            while ( list( $key, $value ) = each( $trackers ) )
-            {
-                if ( is_array( $value->get_plain() ) ) {
-                    while ( list( $key, $value2 ) = each( $value ) )
-                    {
-                        while ( list( $key, $value3 ) = each( $value2 ) )
-                        {
-                            array_push( $trackerlist, $value3->get_plain() );
+            foreach ($trackers as $key => $value) {
+                if (is_array($value->get_plain())) {
+                    foreach ($value as $key => $value2) {
+                        foreach ($value2 as $key => $value3) {
+                            array_push($trackerlist, $value3->get_plain());
                         }
                     }
                 } else {
-                    array_push( $trackerlist, $value->get_plain() );
+                    array_push($trackerlist, $value->get_plain());
                 }
             }
+        } else if ($this->torrent->get_value('announce')) {
+            array_push($trackerlist, $this->torrent->get_value('announce')->get_plain());
         }
-        else if ( $this->torrent->get_value('announce') )
-        {
-            array_push( $trackerlist, $this->torrent->get_value('announce')->get_plain() );
-        }
-        
+
         return $trackerlist;
     }
-      
+
     // Helper function to make adding a tracker easier
     // $tracker_url - string
-    public function addTracker( $tracker_url )
+    public function addTracker($tracker_url)
     {
         $trackers = $this->getTrackers();
         $trackers[] = $tracker_url;
-        $this->setTrackers( $trackers );
+        $this->setTrackers($trackers);
     }
-    
+
     // Replace the current trackers with the supplied list
     // $trackerlist - array of strings
-    public function setTrackers( $trackerlist )
+    public function setTrackers($trackerlist)
     {
-        if ( count( $trackerlist ) >= 1 )
-        {
+        if (count($trackerlist) >= 1) {
             $this->torrent->remove('announce-list');
-            $string = new BEncode_String( $trackerlist[0] );
-            $this->torrent->set( 'announce', $string );
+            $string = new BEncode_String($trackerlist[0]);
+            $this->torrent->set('announce', $string);
         }
-            
-        if ( count( $trackerlist ) > 1 )
-        {
-            $list = new BEncode_List();
-            
 
-            while ( list( $key, $value ) = each( $trackerlist ) )
-            {
+        if (count($trackerlist) > 1) {
+            $list = new BEncode_List();
+
+            foreach ($trackerlist as $key => $value) {
                 $list2 = new BEncode_List();
-                $string = new BEncode_String( $value );
-                $list2->add( $string );
-                $list->add( $list2 );
+                $string = new BEncode_String($value);
+                $list2->add($string);
+                $list->add($list2);
             }
-            
-            $this->torrent->set( 'announce-list', $list );
+
+            $this->torrent->set('announce-list', $list);
         }
     }
-    
+
     // Update the list of files
     // $filelist - array of Torrent_File
-    public function setFiles( $filelist )
+    public function setFiles($filelist)
     {
         // Load files
         $length = $this->info->get_value('length');
 
-        if ( $length )
-        {
-            $filelist[0] = str_replace( '\\', '/', $filelist[0] );
-            $string = new BEncode_String( $filelist[0] );
-            $this->info->set( 'name', $string );
-        }
-        else if ( $this->info->get_value('files') )
-        {
+        if ($length) {
+            $filelist[0] = str_replace('\\', '/', $filelist[0]);
+            $string = new BEncode_String($filelist[0]);
+            $this->info->set('name', $string);
+        } else if ($this->info->get_value('files')) {
             $files = $this->info->get_value('files')->get_plain();
-            for ( $i = 0; $i < count( $files ); ++$i )
-            {
-                $file_parts = split( '/', $filelist[$i] );
+            for ($i = 0; $i < count($files); ++$i) {
+                $file_parts = explode('/', $filelist[$i]);
                 $path = new BEncode_List();
-                foreach ( $file_parts as $part )
-                {
-                    $string = new BEncode_String( $part );
-                    $path->add( $string );
+                foreach ($file_parts as $part) {
+                    $string = new BEncode_String($part);
+                    $path->add($string);
                 }
-                $files[$i]->set( 'path', $path );
+                $files[$i]->set('path', $path);
             }
         }
     }
-    
+
     // Set the comment field
     // $value - string
-    public function setComment( $value )
+    public function setComment($value)
     {
         $type = 'comment';
-        $key = $this->torrent->get_value( $type );
-        if ( $value == '' ) {
-            $this->torrent->remove( $type );
-        } elseif ( $key ) {
-            $key->set( $value );
+        $key = $this->torrent->get_value($type);
+        if ($value == '') {
+            $this->torrent->remove($type);
+        } elseif ($key) {
+            $key->set($value);
         } else {
-            $string = new BEncode_String( $value );
-            $this->torrent->set( $type, $string );
+            $string = new BEncode_String($value);
+            $this->torrent->set($type, $string);
         }
     }
-    
+
     // Set the created by field
     // $value - string
-    public function setCreatedBy( $value )
+    public function setCreatedBy($value)
     {
         $type = 'created by';
-        $key = $this->torrent->get_value( $type );
-        if ( $value == '' ) {
-            $this->torrent->remove( $type );
-        } elseif ( $key ) {
-            $key->set( $value );
+        $key = $this->torrent->get_value($type);
+        if ($value == '') {
+            $this->torrent->remove($type);
+        } elseif ($key) {
+            $key->set($value);
         } else {
-            $string = new BEncode_String( $value );
-            $this->torrent->set( $type, $string );
+            $string = new BEncode_String($value);
+            $this->torrent->set($type, $string);
         }
     }
-    
-    
+
     // Set the creation date
     // $value - php date
-    public function setCreationDate( $value )
+    public function setCreationDate($value)
     {
         $type = 'creation date';
-        $key = $this->torrent->get_value( $type );
-        if ( $value == '' ) {
-            $this->torrent->remove( $type );
-        } elseif ( $key ) {
-            $key->set( $value );
+        $key = $this->torrent->get_value($type);
+        if ($value == '') {
+            $this->torrent->remove($type);
+        } elseif ($key) {
+            $key->set($value);
         } else {
-            $int = new BEncode_Int( $value );
-            $this->torrent->set( $type, $int );
+            $int = new BEncode_Int($value);
+            $this->torrent->set($type, $int);
         }
     }
-    
+
     // Change the private flag
     // $value - -1 public, implicit
     //           0 public, explicit
     //           1 private
-    public function setPrivate( $value )
+    public function setPrivate($value)
     {
-        if ( $value == -1 ) {
-            $this->info->remove( 'private' );
+        if ($value == -1) {
+            $this->info->remove('private');
         } else {
-            $int = new BEncode_Int( $value );
-            $this->info->set( 'private', $int );
+            $int = new BEncode_Int($value);
+            $this->info->set('private', $int);
         }
     }
-    
+
     // Bencode the torrent
     public function bencode()
     {
         return $this->torrent->encode();
     }
-    
+
     // Return the torrent's hash
     public function getHash()
     {
-        return strtoupper( sha1( $this->info->encode() ) );
+        return strtoupper(sha1($this->info->encode()));
     }
 }
 
@@ -325,5 +308,3 @@ class Torrent_File
     public $name;
     public $length;
 }
-
-?>
