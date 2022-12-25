fd02455da4d897dfc7597f4e14144dfb7a16e6c5
Evgen-BioNik <leo.jeka.work@gmail.com>
2022-12-25 19:50:28 +0300

    Added check save settings, when they changed

--- scripts/jquery.common.js
+++ scripts/jquery.common.js
@@ -97,6 +97,7 @@ function doSortSelectByValue(selectID, sortElement = "option") {
 
 // сохранение настроек
 function setSettings() {
+	savecfg.dataset["unsaved"] = 0;
 	var forums = getForums();
 	var tor_clients = getListTorrentClients();
 	var $data = $("#config").serialize();
@@ -119,6 +120,35 @@ function setSettings() {
 			$(this).removeClass("ui-state-disabled").prop("disabled", false);
 		},
 	});
+}
+
+function checkSaveSettings() {
+	let unsaved = !!+savecfg.dataset["unsaved"];
+	if (!unsaved) {
+		return;
+	}
+	$("#dialog").dialog(
+		{
+			buttons: [
+				{
+					text: "Ну и ладно",
+					click: function () {
+						$(this).dialog("close");
+					}
+				},
+				{
+					text: "Сохранить",
+					click: function() {
+						setSettings();
+						$(this).dialog("close");
+					}
+				}
+			],
+			modal: true,
+			resizable: false
+		}
+	).text("Похоже, что вы не сохранили настройки");
+	$("#dialog").dialog("open");
 }
 
 // получение отчётов
