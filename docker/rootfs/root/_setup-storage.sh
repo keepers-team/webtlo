5a78f2325cf4f4640ae7548a4c36cd42bd16a9e3
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-12-27 00:06:19 +0200

    Add a fallback for compatibility with old images

--- docker/rootfs/root/_setup-storage.sh
+++ docker/rootfs/root/_setup-storage.sh
@@ -2,8 +2,19 @@
 
 uid=$(s6-envuidgid -i -u nobody importas UID UID s6-echo '$UID')
 gid=$(s6-envuidgid -i -g nobody importas GID GID s6-echo '$GID')
+legacy=("webtlo.db" "config.ini" "logs")
 
 s6-echo "Creating storage directory in $WEBTLO_DIR"
 s6-mkdir -p -m 0755 "$WEBTLO_DIR"
+
+for name in "${legacy[@]}"; do
+  old="$WEBTLO_DIR/../$name"
+  new="$WEBTLO_DIR/$name"
+  if test -e "$old"; then
+    s6-echo "Found old $name, moving to new location"
+    mv "$old" "$new"
+  fi
+done
+s6-echo "Fixing permissions inside storage directory"
 s6-chown -u "$uid" -g "$gid" "$WEBTLO_DIR"
 s6-chmod 02750 "$WEBTLO_DIR"
