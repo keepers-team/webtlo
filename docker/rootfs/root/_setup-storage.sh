e8899c45ebe8f2db2e73ed3c6b03f6ad1bb159ec
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-12-27 15:31:20 +0200

    Added tfiles directory to recognized files

--- docker/rootfs/root/_setup-storage.sh
+++ docker/rootfs/root/_setup-storage.sh
@@ -2,7 +2,7 @@
 
 uid=$(s6-envuidgid -i -u nobody importas UID UID s6-echo '$UID')
 gid=$(s6-envuidgid -i -g nobody importas GID GID s6-echo '$GID')
-legacy=("webtlo.db" "config.ini" "logs")
+legacy=("webtlo.db" "config.ini" "logs" "tfiles")
 
 s6-echo "Creating storage directory in $WEBTLO_DIR"
 s6-mkdir -p -m 0755 "$WEBTLO_DIR"
