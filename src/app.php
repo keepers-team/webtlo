3d67326b0a0be820571ecf2b4c7bbc3a5f425db9
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-12-28 10:10:30 +0200

    Add dependency injection

--- src/app.php
+++ src/app.php
@@ -1,12 +1,28 @@
 <?php
+
+namespace KeepersTeam\Webtlo;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use KeepersTeam\Webtlo\LegacyRouter;
+use UMA\DIC\Container;
+use Comet\Comet;
 
-$app = new Comet\Comet([
+$webtlo_version = Utils::getVersion();
+$storage_dir = Storage::getStorageDir();
+$db = new DB($storage_dir);
+$ini = new TIniFileEx($storage_dir);
+
+$container = new Container([
+    'webtlo_version' => $webtlo_version,
+    'db' => $db,
+    'ini' => $ini,
+]);
+
+$app = new Comet([
     'host' => '127.0.0.1',
     'port' => 8080,
     'workers' => 4,
+    'container' => $container,
 ]);
 
 
