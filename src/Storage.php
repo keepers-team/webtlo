2d3f31fe319b75a237a2b2d523178c60a612e10b
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-12-28 09:59:07 +0200

    Move storage-related helpers to main namespace

---
+++ src/Storage.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace KeepersTeam\Webtlo;
+
+final class Storage
+{
+    /**
+     * Get normalized path, like realpath() for non-existing path or file
+     *
+     * @param string $path path to be normalized
+     */
+    private static function normalizePath(string $path): string
+    {
+        return array_reduce(explode(DIRECTORY_SEPARATOR, $path), function ($left, $right) {
+            if ($left === null) {
+                return $right;
+            }
+            if ($right === "" || $right === ".") {
+                return $left;
+            }
+            if ($right === "..") {
+                return dirname($left);
+            }
+            $pattern = sprintf("/\%s+/", DIRECTORY_SEPARATOR);
+            return preg_replace($pattern, DIRECTORY_SEPARATOR, $left . DIRECTORY_SEPARATOR . $right);
+        });
+    }
+
+    /**
+     * @return string Storage directory for application
+     */
+    public static function getStorageDir(): string
+    {
+        $directory = getenv('WEBTLO_DIR');
+        if ($directory === false) {
+            return self::normalizePath(__DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . 'data');
+        } else {
+            return $directory;
+        }
+    }
+
+
+    /**
+     * @return string The log directory for the application
+     * @deprecated Will be replaced with Monolog
+     */
+    public static function getLogDir(): string
+    {
+        return self::getStorageDir() . DIRECTORY_SEPARATOR . "logs";
+    }
+}
