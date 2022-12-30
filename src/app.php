63a1e0f44992ebf7b0007bae5c4eb95525c14033
Andriy Kushnir (Orhideous) <me@orhideous.name>
2022-12-30 16:17:18 +0200

    Allow channels in logger

--- src/app.php
+++ src/app.php
@@ -16,23 +16,23 @@ $storage_dir = Storage::getStorageDir();
 $db = new DB($storage_dir);
 $ini = new TIniFileEx($storage_dir);
 
-function configureLogger(): Logger
+function configureLogger(string $name): Logger
 {
     $logsDirectory = Storage::getStorageDir() . DIRECTORY_SEPARATOR . "logs";
-    $logName = $logsDirectory . DIRECTORY_SEPARATOR . 'webtlo.log';
+    $logName = $logsDirectory . DIRECTORY_SEPARATOR . 'application.log';
     Utils::mkdir_recursive($logsDirectory);
 
     $dateFormat = "Y-m-d\TH:i:s";
-    $output = "[%datetime%] %level_name%: %message% %context%\n";
+    $output = "[%datetime%] %channel% %level_name%: %message% %context%\n";
     $formatter = new LineFormatter($output, $dateFormat);
 
-    $logger = new Logger('webtlo');
+    $logger = new Logger($name);
     $logger->pushHandler((new RotatingFileHandler($logName, 30))->setFormatter($formatter));
     $logger->pushHandler((new StreamHandler('php://stdout'))->setFormatter($formatter));
     return $logger;
 }
 
-$logger = configureLogger();
+$logger = configureLogger('webtlo');
 
 $container = new Container([
     'webtlo_version' => $webtlo_version,
