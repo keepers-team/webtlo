d1a0f83053505e4d669fd5207f4a6d3fa6217b89
Andriy Kushnir (Orhideous) <me@orhideous.name>
2023-01-15 00:26:00 +0200

    Extracted shared code for http requests

--- src/Forum/TorrentDownloader.php
+++ src/Forum/TorrentDownloader.php
@@ -2,106 +2,38 @@
 
 namespace KeepersTeam\Webtlo\Forum;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Header;
-use GuzzleRetry\GuzzleRetryMiddleware;
 use KeepersTeam\Webtlo\Config\Defaults;
 use KeepersTeam\Webtlo\Config\Proxy;
 use KeepersTeam\Webtlo\Config\Timeout;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamInterface;
 use Psr\Log\LoggerInterface;
 
-class TorrentDownloader
+class TorrentDownloader extends WebClient
 {
-    use ProxySupport;
-
-    private readonly Client $client;
-    private static string $torrentMime = 'application/x-bittorrent';
     private static string $url = '/forum/dl.php';
     private readonly array $defaultParams;
 
     public function __construct(
-        private readonly LoggerInterface $logger,
         string $apiKey,
         string $userID,
         bool $addRetracker,
+        LoggerInterface $logger,
+        ?Proxy $proxy = null,
         string $forumURL = Defaults::forumUrl,
         Timeout $timeout = new Timeout(),
-        ?Proxy $proxy = null
     ) {
+        parent::__construct(
+            logger: $logger,
+            proxy: $proxy,
+            forumURL: $forumURL,
+            timeout: $timeout
+        );
         $this->defaultParams = [
             'keeper_user_id' => $userID,
             'keeper_api_key' => $apiKey,
             'add_retracker_url' => $addRetracker ? 1 : 0,
         ];
-
-        $retryCallback = function (int $attemptNumber, float $delay, RequestInterface &$request, array &$options, ?ResponseInterface $response) use ($logger): void {
-            $logger->warning(
-                'Retrying request',
-                [
-                    'url' => $request->getUri()->__toString(),
-                    'delay' => number_format($delay, 2),
-                    'attempt' => $attemptNumber
-                ]
-            );
-        };
-        $retryOptions = [
-            'max_retry_attempts' => 3,
-            'retry_on_timeout' => true,
-            'on_retry_callback' => $retryCallback,
-        ];
-
-        $headers = [
-            'User-Agent' => Defaults::userAgent,
-            'Accept' => self::$torrentMime,
-            'X-WebTLO' => 'experimental'
-        ];
-
-        $stack = HandlerStack::create();
-        $stack->push(GuzzleRetryMiddleware::factory($retryOptions));
-        $baseUrl = sprintf("https://%s%s", $forumURL, self::$url);
-        $proxyConfig = static::getProxyConfig($this->logger, $proxy);
-        $this->client = new Client([
-            ...$proxyConfig,
-            'base_uri' => $baseUrl,
-            'timeout' => $timeout->request,
-            'connect_timeout' => $timeout->connection,
-            'allow_redirects' => true,
-            'headers' => $headers,
-            'handler' => $stack
-        ]);
-        $logger->info('Created downloader', ['base' => $baseUrl]);
-    }
-
-    /**
-     * Check response for correctness
-     *
-     * @param ResponseInterface $response
-     * @return bool
-     */
-    private function isValid(ResponseInterface &$response): bool
-    {
-        $type = $response->getHeader('content-type');
-        if (empty($type)) {
-            $this->logger->warning('No content-type found');
-            return false;
-        }
-        $parsed = Header::parse($type);
-        if (!isset($parsed[0][0])) {
-            $this->logger->warning('Broken content-type header');
-            return false;
-        }
-        $receivedMime = $parsed[0][0];
-
-        if ($receivedMime !== self::$torrentMime) {
-            $this->logger->warning('Unknown mime', ['expected' => self::$torrentMime, 'received' => $receivedMime]);
-            return false;
-        }
-        return true;
     }
 
     /**
@@ -123,7 +55,7 @@ class TorrentDownloader
             return false;
         }
 
-        if ($this->isValid($response)) {
+        if ($this->isValidMime($response, self::torrentMime)) {
             return $response->getBody();
         } else {
             return false;
---
+++ src/Forum/WebClient.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace KeepersTeam\Webtlo\Forum;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Header;
+use GuzzleRetry\GuzzleRetryMiddleware;
+use KeepersTeam\Webtlo\Config\Defaults;
+use KeepersTeam\Webtlo\Config\Proxy;
+use KeepersTeam\Webtlo\Config\Timeout;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+abstract class WebClient
+{
+    use ProxySupport;
+
+    protected const torrentMime = 'application/x-bittorrent';
+    protected const webMime = 'text/html';
+
+    /**
+     * @var array<string, string>
+     */
+    private const clientHeaders = [
+        'User-Agent' => Defaults::userAgent,
+        'X-WebTLO' => 'experimental'
+    ];
+
+    protected readonly Client $client;
+    protected readonly CookieJar $cookieJar;
+
+    public function __construct(
+        protected readonly LoggerInterface $logger,
+        ?Proxy $proxy = null,
+        string $forumURL = Defaults::forumUrl,
+        Timeout $timeout = new Timeout(),
+    ) {
+        $retryCallback = function (int $attemptNumber, float $delay, RequestInterface &$request, array &$options, ?ResponseInterface $response) use ($logger): void {
+            $logger->warning(
+                'Retrying request',
+                [
+                    'url' => $request->getUri()->__toString(),
+                    'delay' => number_format($delay, 2),
+                    'attempt' => $attemptNumber
+                ]
+            );
+        };
+        $retryOptions = [
+            'max_retry_attempts' => 3,
+            'retry_on_timeout' => true,
+            'on_retry_callback' => $retryCallback,
+        ];
+
+        $stack = HandlerStack::create();
+        $stack->push(GuzzleRetryMiddleware::factory($retryOptions));
+        $baseUrl = sprintf("https://%s", $forumURL);
+        $proxyConfig = static::getProxyConfig($this->logger, $proxy);
+        $this->cookieJar = new CookieJar();
+        $this->client = new Client([
+            ...$proxyConfig,
+            'base_uri' => $baseUrl,
+            'timeout' => $timeout->request,
+            'connect_timeout' => $timeout->connection,
+            'allow_redirects' => true,
+            'headers' => self::clientHeaders,
+            'handler' => $stack,
+            'cookies' => $this->cookieJar,
+        ]);
+        $logger->info('Created client', ['base' => $baseUrl]);
+    }
+
+    /**
+     * Check response for correctness
+     *
+     * @param ResponseInterface $response Received response
+     * @param string $expectedMime Expected MIME
+     * @return bool
+     */
+    protected function isValidMime(ResponseInterface $response, string $expectedMime): bool
+    {
+        $type = $response->getHeader('content-type');
+        if (empty($type)) {
+            $this->logger->warning('No content-type found');
+            return false;
+        }
+        $parsed = Header::parse($type);
+        if (!isset($parsed[0][0])) {
+            $this->logger->warning('Broken content-type header');
+            return false;
+        }
+        $receivedMime = $parsed[0][0];
+
+        if ($receivedMime !== $expectedMime) {
+            $this->logger->warning('Unknown mime', ['expected' => $expectedMime, 'received' => $receivedMime]);
+            return false;
+        }
+        return true;
+    }
+}
