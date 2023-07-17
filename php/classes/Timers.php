a0f033cbb6e860149f51b8f3dd8689ba3f1115e8
NeBioNik <nebionik@users.noreply.github.com>
2023-07-17 19:59:48 +0300

    Back. Add Timers class

---
+++ php/classes/Timers.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Простой способ замера времени выполнения.
+ */
+class Timers
+{
+    private static array $markers = [];
+
+    public static function start(string $marker = 'default'): void
+    {
+        self::$markers[$marker]['start'] = microtime(true);
+    }
+
+    public static function stop(string $marker = 'default'): void
+    {
+        self::$markers[$marker]['stop'] = $stop = microtime(true);
+
+        $start = self::$markers[$marker]['start'] ?? $stop;
+
+        self::$markers[$marker]['exec'] = $stop - $start;
+    }
+
+    public static function getExec(string $marker = 'default'): int
+    {
+        self::stop($marker);
+
+        return floor(self::$markers[$marker]['exec']);
+    }
+
+    public static function getExecTime(string $marker = 'default', bool $leadZeros = false): string
+    {
+        self::stop($marker);
+
+        return convert_seconds(self::$markers[$marker]['exec'] ?? 0, $leadZeros);
+    }
+
+    public static function printExecTime(string $marker = 'default'): void
+    {
+        echo sprintf('ExecTime [%s]: %s<br>', $marker, self::getExecTime($marker));
+    }
+}
