ff4dd501ec27412bd255ca86e1e5cbbe5e693f90
NeBioNik <nebionik@users.noreply.github.com>
2023-07-18 16:17:37 +0300

    ForumTreeProcess. Refactor

--- php/common/forum_tree.php
+++ php/common/forum_tree.php
@@ -3,6 +3,16 @@
 include_once dirname(__FILE__) . '/../common.php';
 include_once dirname(__FILE__) . '/../classes/api.php';
 
+Timers::start('forum_tree');
+/** Ид подраздела обновления дерева подразделов  */
+const FORUM_TREE_UPDATE = 8888;
+
+// Проверяем время последнего обновления.
+if (!check_update_available(FORUM_TREE_UPDATE)) {
+    Log::append('Обновление списка подразделов не требуется.');
+    return;
+}
+
 // получение настроек
 if (!isset($cfg)) {
     $cfg = get_settings();
@@ -15,88 +25,77 @@ if (!isset($api)) {
     $api->setUserConnectionOptions($cfg['curl_setopt']['api']);
 }
 
-// обновление дерева подразделов
-$forum_tree_update = Db::query_database(
-    "SELECT strftime('%s', 'now') - ud FROM UpdateTime WHERE id = ?",
-    [8888],
-    true,
-    PDO::FETCH_COLUMN
-);
+// получение дерева подразделов
+$forumTree = $api->getCategoryForumTree();
 
-if (
-    empty($forum_tree_update)
-    || $forum_tree_update[0] > 3600
-) {
-    // получение дерева подразделов
-    $forum_tree = $api->getCategoryForumTree();
+if (empty($forumTree['result'])) {
+    throw new Exception("Error: Не удалось получить дерево подразделов");
+}
 
-    if (empty($forum_tree['result'])) {
-        throw new Exception("Error: Не удалось получить дерево подразделов");
-    }
+$treeUpdateTime = $forumTree['update_time'];
 
-    $forum_tree_update_current = $forum_tree['update_time'];
-
-    foreach ($forum_tree['result']['c'] as $cat_id => $cat_title) {
-        foreach ($forum_tree['result']['tree'][$cat_id] as $forum_id => $subforum) {
-            // разделы
-            $forum_title = $cat_title . ' » ' . $forum_tree['result']['f'][$forum_id];
-            $forums[$forum_id] = [
-                'na' => $forum_title,
+foreach ($forumTree['result']['c'] as $catId => $catTitle) {
+    foreach ($forumTree['result']['tree'][$catId] as $forum_id => $subForum) {
+        // разделы
+        $forumTitle = $catTitle . ' » ' . $forumTree['result']['f'][$forum_id];
+        $forums[$forum_id] = [
+            'na' => $forumTitle,
+            'qt' => 0,
+            'si' => 0,
+        ];
+        // подразделы
+        foreach ($subForum as $subForumId) {
+            $subForumTitle = $catTitle . ' » ' . $forumTree['result']['f'][$forum_id] . ' » ' . $forumTree['result']['f'][$subForumId];
+            $forums[$subForumId] = [
+                'na' => $subForumTitle,
                 'qt' => 0,
                 'si' => 0,
             ];
-            // подразделы
-            foreach ($subforum as $subforum_id) {
-                $subforum_title = $cat_title . ' » ' . $forum_tree['result']['f'][$forum_id] . ' » ' . $forum_tree['result']['f'][$subforum_id];
-                $forums[$subforum_id] = [
-                    'na' => $subforum_title,
-                    'qt' => 0,
-                    'si' => 0,
-                ];
-            }
+            unset($subForumId, $subForumTitle);
         }
+        unset($forum_id, $forumTitle, $subForum);
     }
-    unset($forum_tree);
+    unset($catId, $catTitle);
+}
+unset($forumTree);
 
-    // получение количества и веса раздач по разделам
-    $forum_size = $api->getCategoryForumVolume();
+// получение количества и веса раздач по разделам
+$forum_size = $api->getCategoryForumVolume();
 
-    if (empty($forum_size['result'])) {
-        throw new Exception("Error: Не удалось получить количество и вес раздач по разделам");
+if (empty($forum_size['result'])) {
+    throw new Exception("Error: Не удалось получить количество и вес раздач по разделам");
+}
+
+foreach ($forum_size['result'] as $forum_id => $values) {
+    if (empty($values)) {
+        continue;
     }
-
-    $forum_size_update_current = $forum_size['update_time'];
-
-    foreach ($forum_size['result'] as $forum_id => $values) {
-        if (empty($values)) {
-            continue;
-        }
-        if (isset($forums[$forum_id])) {
-            $forums[$forum_id] = array_merge(
-                $forums[$forum_id],
-                array_combine(
-                    ['qt', 'si'],
-                    $values
-                )
-            );
-        }
+    if (isset($forums[$forum_id])) {
+        $forums[$forum_id] = array_merge(
+            $forums[$forum_id],
+            array_combine(
+                ['qt', 'si'],
+                $values
+            )
+        );
     }
+}
 
+if (isset($forums) && count($forums)) {
     // создаём временную таблицу
     Db::query_database(
         'CREATE TEMP TABLE ForumsNew AS
-        SELECT id,na,qt,si FROM Forums WHERE 0 = 1'
+    SELECT id,na,qt,si FROM Forums WHERE 0 = 1'
     );
 
     // отправляем в базу данных
-    $forums = array_chunk($forums, 500, true);
+    $forumsChunks = array_chunk($forums, 500, true);
 
-    foreach ($forums as $forums) {
-        $select = Db::combine_set($forums);
+    foreach ($forumsChunks as $forumsParts) {
+        $select = Db::combine_set($forumsParts);
         Db::query_database("INSERT INTO temp.ForumsNew (id,na,qt,si) $select");
-        unset($select);
+        unset($forumsParts, $select);
     }
-    unset($forums);
 
     Log::append("Обновление дерева подразделов...");
 
@@ -108,12 +107,14 @@ if (
         WHERE temp.ForumsNew.id IS NULL
     )');
 
-    // время обновления дерева подразделов
-    Db::query_database(
-        "INSERT INTO UpdateTime (id,ud) SELECT 8888,?",
-        [$forum_tree_update_current]
-    );
 
-    unset($forum_tree_update_current);
-    unset($forum_tree_update);
+    // Записываем время обновления.
+    set_last_update_time(FORUM_TREE_UPDATE, $treeUpdateTime);
+
+    Log::append(sprintf(
+        'Обновление дерева подразделов завершено за %s, обработано подразделов: %d шт',
+        Timers::getExecTime('forum_tree'),
+        count($forums)
+    ));
+    unset($forums, $forumsChunks, $treeUpdateTime);
 }
