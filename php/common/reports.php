9f7ea3a8fb52a7d94424da061fef7ffbd422f3f2
NeBioNik <nebionik@users.noreply.github.com>
2023-07-19 13:17:46 +0300

    Reports Cron. Use ReportCreator class for cron reports creation

--- php/common/reports.php
+++ php/common/reports.php
@@ -1,62 +1,28 @@
 <?php
 
-$starttime = microtime(true);
-
 include_once dirname(__FILE__) . '/../common.php';
 include_once dirname(__FILE__) . '/../classes/reports.php';
+include_once dirname(__FILE__) . '/../classes/ReportCreator.php';
 include_once dirname(__FILE__) . '/../classes/user_details.php';
 
-Log::append("Начат процесс отправки отчётов...");
+Timers::start('send_reports');
+Log::append('Начат процесс отправки отчётов...');
 
 // получение настроек
 $cfg = get_settings();
 
 // проверка настроек
 if (empty($cfg['subsections'])) {
-    throw new Exception("Error: Не выбраны хранимые подразделы");
+    throw new Exception('Error: Не выбраны хранимые подразделы');
 }
 
 if (empty($cfg['tracker_login'])) {
-    throw new Exception("Error: Не указано имя пользователя для доступа к форуму");
+    throw new Exception('Error: Не указано имя пользователя для доступа к форуму');
 }
 
 if (empty($cfg['tracker_paswd'])) {
-    throw new Exception("Error: Не указан пароль пользователя для доступа к форуму");
+    throw new Exception('Error: Не указан пароль пользователя для доступа к форуму');
 }
-
-// update_time[0] время последнего обновления сведений
-$update_time = Db::query_database(
-    "SELECT ud FROM UpdateTime WHERE id = 7777",
-    [],
-    true,
-    PDO::FETCH_COLUMN
-);
-
-if (empty($update_time[0])) {
-    throw new Exception("Error: Отсутствует метка времени последнего обновления сведений");
-}
-
-// исключаемые подразделы и торрент-клиенты
-$excludeForumsIDs  = explode(',', $cfg['reports']['exclude_forums_ids']);
-$excludeClientsIDs = explode(',', $cfg['reports']['exclude_clients_ids']);
-if (!empty($cfg['reports']['exclude_clients_ids'])) {
-    Log::append("Notice: Из отчёта исключены торрент клиенты: {$cfg['reports']['exclude_clients_ids']}");
-}
-
-// const & pattern
-$message_length_max = 119000;
-$pattern_topic = '[url=viewtopic.php?t=%s]%s[/url] %s';
-$pattern_spoiler = '[spoiler="№№ %s — %s"][list=1][*=%s]%s[/list][/spoiler]';
-$pattern_common = '[url=viewtopic.php?t=%s][u]%s[/u][/url] — %s шт. (%s)';
-$pattern_header = 'Хранитель %s: [url=profile.php?mode=viewprofile&u=%s&name=1][u][color=#006699]%s[/u][/color][/url] [color=gray]~>[/color] %s шт. [color=gray]~>[/color] %s[br]';
-$spoiler_length = mb_strlen($pattern_spoiler, 'UTF-8');
-
-// общий объём и количество хранимого в сводный
-$sumdlqt = 0;
-$sumdlsi = 0;
-
-// список подразделов в сводный
-$common_forums = [];
 
 // подключаемся к форуму
 $reports = new Reports(
@@ -64,334 +30,101 @@ $reports = new Reports(
     $cfg['tracker_login'],
     $cfg['tracker_paswd']
 );
-
 // применяем таймауты
 $reports->curl_setopts($cfg['curl_setopt']['forum']);
 
+// Создание отчётов.
+$forumReports = new ReportCreator(
+    $cfg,
+    $webtlo,
+    $reports
+);
+
+$editedTopicsIDs = [];
 foreach ($cfg['subsections'] as $forum_id => $subsection) {
-    // Log::append("Отправка отчётов для подраздела № $forum_id...");
+    Timers::start("send_$forum_id");
 
-    // исключаем подразделы
-    if (in_array($forum_id, $excludeForumsIDs)) {
-        Log::append("Notice: Из отчёта исключен подраздел № $forum_id");
-        continue;
-    }
+    $forumReport = $forumReports->getForumReport($forum_id);
 
-    // получение данных о подразделе
-    $forum = Db::query_database(
-        "SELECT * FROM Forums WHERE id = ?",
-        [$forum_id],
-        true,
-        PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE
-    );
+    $messages = $forumReport['messages'];
+    $topicParams = $forumReports->getTopicSavedParams($forum_id);
+    // Log::append(sprintf('forum_id: %d => %s', $forum_id, json_encode($topicParams, JSON_UNESCAPED_UNICODE)));
 
-    if (empty($forum)) {
-        Log::append("Error: Не получены данные о хранимом подразделе № $forum_id");
-        continue;
-    }
-
-    // получение данных о раздачах
-    $client_exclude = str_repeat('?,', count($excludeClientsIDs) - 1) . '?';
-    $topics = Db::query_database(
-        'SELECT
-            Topics.id,
-            Topics.ss,
-            Topics.na,
-            Topics.si,
-            Topics.st,
-            Torrents.done
-        FROM Topics
-        LEFT JOIN (
-            SELECT
-                info_hash,
-                MAX(done) AS done
-            FROM Torrents
-            WHERE error = 0
-                AND client_id NOT IN (' . $client_exclude . ')
-            GROUP BY info_hash
-        ) Torrents ON Topics.hs = Torrents.info_hash
-        WHERE
-            Torrents.info_hash IS NOT NULL
-            AND Topics.ss = ?',
-        array_merge($excludeClientsIDs, [$forum_id]),
-        true
-    );
-
-    if (empty($topics)) {
-        Log::append("Notice: Не получены данные о хранимых раздачах для подраздела № $forum_id");
-        continue;
-    }
-
-    // сортировка раздач
-    $topics = natsort_field($topics, 'na');
-
-    // количество раздач
-    $topics_count = count($topics);
-    Log::append("Найдено хранимых раздач $topics_count шт для подраздела № $forum_id ");
-
-    // очищаем данные в цикле
-    $posts_ids = [];
-    $stored = [];
-    $tmp = [];
-
-    // формируем списки
-    foreach ($topics as $topic) {
-        if (empty($tmp)) {
-            $tmp['start'] = 1;
-            $tmp['lgth'] = 0;
-            $tmp['dlsi'] = 0;
-            $tmp['dlqt'] = 0;
-            $tmp['dlsisub'] = 0;
-            $tmp['dlqtsub'] = 0;
-        }
-        $topicLink = $topic['done'] != 1 ? $topic['id'] . '#dl' : $topic['id'];
-        $str = sprintf(
-            $pattern_topic,
-            $topicLink,
-            $topic['na'],
-            convert_bytes($topic['si'])
+    $topicId = $topicParams['topicId'];
+    // Редактируем шапку темы, если её автор - пользователь.
+    if (strcasecmp($cfg['tracker_login'], $topicParams['authorNickName']) === 0) {
+        Log::append(sprintf('Отправка шапки, ид темы %d, ид сообщения %d', $topicId, $topicParams['authorPostId']));
+        // отправка сообщения с шапкой
+        $reports->send_message(
+            'editpost',
+            $forumReport['header'],
+            $topicId,
+            $topicParams['authorPostId'],
+            '[Список] ' . $subsection['na']
         );
-        if ($topic['done'] != 1) {
-            $tmp['dlqtsub']++;
-            $tmp['dlsisub'] += $topic['si'];
-            $str .= ' :!: ';
-        } else {
-            $tmp['dlsi'] += $topic['si'];
-        }
-        $tmp['dlqt']++;
-        $lgth = mb_strlen($str, 'UTF-8');
-        $tmp['str'][] = $str;
-        $tmp['lgth'] += $lgth;
-        $current_length = $tmp['lgth'] + $lgth;
-        $available_length = $message_length_max - $spoiler_length - ($tmp['dlqt'] - $tmp['start'] + 1) * 3;
-        if (
-            $current_length > $available_length
-            || $tmp['dlqt'] == $topics_count
-        ) {
-            $tmp['str'] = implode('[*]', $tmp['str']);
-            $tmp['msg'][] = sprintf(
-                $pattern_spoiler,
-                $tmp['start'],
-                $tmp['dlqt'],
-                $tmp['start'],
-                $tmp['str']
-            );
-            $tmp['start'] = $tmp['dlqt'] + 1;
-            $tmp['lgth'] = 0;
-            unset($tmp['str']);
-        }
+        usleep(500);
     }
-    unset($topics);
-
-    if (empty($tmp['msg'])) {
-        Log::append("Error: Не удалось сформировать список хранимого для подраздела № $forum_id");
-        continue;
-    }
-
-    // вычитаем раздачи на загрузке
-    $tmp['dlqt'] -= $tmp['dlqtsub'];
-
-    // дописываем в начало первого сообщения
-    $tmp['msg'][0] = 'Актуально на: [color=darkblue]' . date('d.m.Y', $update_time[0]) . '[/color][br]' .
-        'Всего хранимых раздач в подразделе: ' . $tmp['dlqt'] . ' шт. / ' . convert_bytes($tmp['dlsi']) . '[br]' .
-        'Всего скачиваемых раздач в подразделе: ' . $tmp['dlqtsub'] . ' шт. / ' . convert_bytes($tmp['dlsisub']) . '[br]' .
-        $webtlo->version_line .
-        $tmp['msg'][0];
-
-    // ищем тему со списками
-    $topicID = $reports->search_topic_id($forum[$forum_id]['na']);
-
-    if (empty($topicID)) {
-        Log::append("Error: Не удалось найти тему со списком для подраздела № $forum_id");
-        continue;
-    }
-
-    // сохраним все редактирумые темы
-    $editedTopicsIDs[] = $topicID;
-
-    // Log::append("Сканирование списков...");
-
-    // сканируем имеющиеся списки
-    $keepers = $reports->scanning_viewtopic($topicID);
-
-    if ($keepers !== false) {
-        // разбираем инфу, полученную из списков
-        foreach ($keepers as $index => $keeper) {
-            $posted = $keeper['posted'];
-            // array( 'post_id', 'nickname', 'posted', 'topics_ids' => array(...) )
-            if ($index == 0) {
-                $postIDAuthorTopic = $keeper['post_id'];
-                $nicknameAuthorTopic = $keeper['nickname'];
-                continue;
-            }
-            // запоминаем свои сообщения
-            if (strcasecmp($cfg['tracker_login'], $keeper['nickname']) === 0) {
-                $posts_ids[] = $keeper['post_id'];
-                continue;
-            }
-            // если хранитель не является автором шапки
-            if (strcasecmp($cfg['tracker_login'], $nicknameAuthorTopic) !== 0) {
-                continue;
-            }
-            // считаем сообщения других хранителей в подразделе
-            if (!isset($stored[$keeper['nickname']])) {
-                $stored[$keeper['nickname']]['dlqt'] = 0;
-                $stored[$keeper['nickname']]['dlsi'] = 0;
-                $stored[$keeper['nickname']]['dlqtsub'] = 0;
-                $stored[$keeper['nickname']]['dlsisub'] = 0;
-            }
-            if (empty($keeper['topics_ids'])) {
-                continue;
-            }
-            foreach ($keeper['topics_ids'] as $index => $keeperTopicsIDs) {
-                $topics_ids = array_chunk($keeperTopicsIDs, 500);
-                foreach ($topics_ids as $topics_ids) {
-                    $in = str_repeat('?,', count($topics_ids) - 1) . '?';
-                    $values = Db::query_database(
-                        "SELECT COUNT(),SUM(si) FROM Topics
-                            WHERE id IN ($in) AND ss = $forum_id AND rg < CAST($posted as INTEGER)",
-                        $topics_ids,
-                        true,
-                        PDO::FETCH_NUM
-                    );
-                    if ($index == 1) {
-                        $stored[$keeper['nickname']]['dlqt'] += $values[0][0];
-                        $stored[$keeper['nickname']]['dlsi'] += $values[0][1];
-                    } else {
-                        $stored[$keeper['nickname']]['dlqtsub'] += $values[0][0];
-                        $stored[$keeper['nickname']]['dlsisub'] += $values[0][1];
-                    }
-                    unset($values);
-                }
-                unset($topics_ids);
-            }
-        }
-    }
-
-    // Log::append("Найдено своих сообщений: " . count($posts_ids));
 
     // вставка доп. сообщений
-    if (count($tmp['msg']) > count($posts_ids)) {
-        $count_post_reply = count($tmp['msg']) - count($posts_ids);
+    $postList = $topicParams['postList'];
+    if (count($messages) > count($postList)) {
+        $count_post_reply = count($messages) - count($postList);
         for ($i = 1; $i <= $count_post_reply; $i++) {
             // Log::append("Вставка дополнительного $i-ого сообщения...");
             $message = '[spoiler]' . $i . str_repeat('?', 119981 - mb_strlen($i)) . '[/spoiler]';
-            $posts_ids[] = $reports->send_message(
+            $postList[] = $reports->send_message(
                 'reply',
                 $message,
-                $topicID
+                $topicId
             );
-            usleep(1500);
+            usleep(500);
         }
     }
 
     // редактирование сообщений
-    foreach ($posts_ids as $index => $post_id) {
+    foreach ($postList as $index => $postId) {
         $post_number = $index + 1;
         // Log::append("Редактирование сообщения № $post_number...");
-        $message = empty($tmp['msg'][$index]) ? 'резерв' : $tmp['msg'][$index];
+        $message = $messages[$index] ?? 'резерв';
         $reports->send_message(
             'editpost',
             $message,
-            $topicID,
-            $post_id
+            $topicId,
+            $postId
         );
+
+        unset($index, $postId, $post_number, $message);
     }
 
-    // работа с шапкой
-    if (strcasecmp($cfg['tracker_login'], $nicknameAuthorTopic) === 0) {
-        $tmp['header'] = '[url=viewforum.php?f=' . $forum_id . '][u][color=#006699]' . preg_replace('/.*» ?(.*)$/', '$1', $forum[$forum_id]['na']) . '[/u][/color][/url] ' .
-            '| [url=tracker.php?f=' . $forum_id . '&tm=-1&o=10&s=1&oop=1][color=indigo][u]Проверка сидов[/u][/color][/url][br][br]' .
-            'Актуально на: [color=darkblue]' . date('d.m.Y', $update_time[0]) . '[/color][br]' .
-            'Всего раздач в подразделе: ' . $forum[$forum_id]['qt'] . ' шт. / ' . convert_bytes($forum[$forum_id]['si']) . '[br]' .
-            'Всего хранимых раздач в подразделе: %%dlqt%% шт. / %%dlsi%%[br]' .
-            'Всего скачиваемых раздач в подразделе: %%dlqtsub%% шт. / %%dlsisub%%[br]' .
-            'Количество хранителей: %%kpqt%%[hr]' .
-            'Хранитель 1: [url=profile.php?mode=viewprofile&u=' . urlencode($nicknameAuthorTopic) . '&name=1][u][color=#006699]' . $nicknameAuthorTopic . '[/u][/color][/url] [color=gray]~>[/color] ' . $tmp['dlqt'] . ' шт. [color=gray]~>[/color] ' . convert_bytes($tmp['dlsi']) . '[br]';
-        // значения хранимого для шапки
-        $count_keepers = 1;
-        $sumdlqt_keepers = $tmp['dlqt'];
-        $sumdlsi_keepers = $tmp['dlsi'];
-        $sumdlqtsub_keepers = $tmp['dlqtsub'];
-        $sumdlsisub_keepers = $tmp['dlsisub'];
-        // учитываем хранимое другими
-        foreach ($stored as $nickname => $values) {
-            $count_keepers++;
-            $tmp['header'] .= sprintf(
-                $pattern_header,
-                $count_keepers,
-                urlencode($nickname),
-                $nickname,
-                $values['dlqt'],
-                convert_bytes($values['dlsi'])
-            );
-            $sumdlqt_keepers += $values['dlqt'];
-            $sumdlsi_keepers += $values['dlsi'];
-            $sumdlqtsub_keepers += $values['dlqtsub'];
-            $sumdlsisub_keepers += $values['dlsisub'];
-        }
-        // вставляем общее хранимое в шапку
-        $tmp['header'] = str_replace(
-            [
-                '%%dlqt%%',
-                '%%dlsi%%',
-                '%%dlqtsub%%',
-                '%%dlsisub%%',
-                '%%kpqt%%',
-            ],
-            [
-                $sumdlqt_keepers,
-                convert_bytes($sumdlsi_keepers),
-                $sumdlqtsub_keepers,
-                convert_bytes($sumdlsisub_keepers),
-                $count_keepers,
-            ],
-            $tmp['header']
-        );
-        // Log::append('Отправка шапки...');
-        // отправка сообщения с шапкой
-        $reports->send_message(
-            'editpost',
-            $tmp['header'],
-            $topicID,
-            $postIDAuthorTopic,
-            '[Список] ' . $subsection['na']
-        );
-    }
-
-    // добавляем информацию в сводный отчёт
-    $sumdlqt += $tmp['dlqt'];
-    $sumdlsi += $tmp['dlsi'];
-    $common_forums[$forum_id] = sprintf(
-        $pattern_common,
-        $topicID,
-        $forum[$forum_id]['na'],
-        $tmp['dlqt'],
-        convert_bytes($tmp['dlsi'])
-    );
+    $editedTopicsIDs[] = $topicId;
+    Log::append(sprintf(
+        'Отправка отчёта для подраздела № %d завершена за %s. Сообщений отредактировано %d.',
+        $forum_id,
+        Timers::getExecTime("send_$forum_id"),
+        count($postList)
+    ));
 }
 
-Log::append("Обработано подразделов: " . count($common_forums) . " шт.");
+Log::append("Обработано подразделов: " . count($editedTopicsIDs) . " шт.");
 
 // работаем со сводным отчётом
 if ($cfg['reports']['send_summary_report']) {
+    Timers::start('send_summary');
     // формируем сводный отчёт
-    $common = 'Актуально на: [b]' . date('d.m.Y', $update_time[0]) . '[/b][br][br]' .
-        'Общее количество хранимых раздач: [b]' . $sumdlqt . '[/b] шт.[br]' .
-        'Общий вес хранимых раздач: [b]' . preg_replace('/ (?!.* )/', '[/b] ', convert_bytes($sumdlsi)) . '[br]' .
-        $webtlo->version_line_url . '[hr]' .
-        implode('[br]', $common_forums);
+    $summaryReport = $forumReports->getSummaryReport();
+
     // ищем сообщение со сводным
-    $post_id = $reports->search_post_id(4275633, true);
-    $common_mode = empty($post_id) ? 'reply' : 'editpost';
-    Log::append("Отправка сводного отчёта...");
+    $summaryPostId = $reports->search_post_id(4275633, true);
+
+    $summaryPostMode = empty($summaryPostId) ? 'reply' : 'editpost';
     // отправляем сводный отчёт
     $reports->send_message(
-        $common_mode,
-        $common,
+        $summaryPostMode,
+        $summaryReport,
         4275633,
-        $post_id
+        $summaryPostId
     );
+    Log::append(sprintf('Отправка сводного отчёта завершена за %s', Timers::getExecTime('send_summary')));
 }
 
 // отредактируем все сторонние темы со своими сообщениями в рабочем подфоруме
@@ -399,6 +132,7 @@ if (
     $cfg['reports']['auto_clear_messages']
     && !empty($editedTopicsIDs)
 ) {
+    $emptyMessages = [];
     $topicsIDsWithMyMessages = $reports->searchTopicsIDs(['uid' => $cfg['user_id']]);
     $uneditedTopicsIDs = array_diff($topicsIDsWithMyMessages, $editedTopicsIDs);
     if (!empty($uneditedTopicsIDs)) {
@@ -414,13 +148,20 @@ if (
                 }
                 // только свои сообщения
                 if (strcasecmp($cfg['tracker_login'], $message['nickname']) === 0) {
+                    $emptyMessages[] = $message['post_id'];
                     $reports->send_message('editpost', ':!: не актуально', $topicID, $message['post_id']);
                 }
             }
         }
     }
+
+    if (count($emptyMessages)) {
+        Log::append(sprintf(
+            'Помечено неактуальных сообщений: %d => %s',
+            count($emptyMessages),
+            implode(',', $emptyMessages)
+        ));
+    }
 }
 
-$endtime = microtime(true);
-
-Log::append("Завершён процесс отправки отчётов за " . convert_seconds($endtime - $starttime));
+Log::append(sprintf('Процесс отправки отчётов завершён за %s', Timers::getExecTime('send_reports')));
