41b902a3c89661f3afa08f764b57ac3ae99df198
NeBioNik <nebionik@users.noreply.github.com>
2023-07-28 15:00:43 +0300

    Log. Show in natural order

--- php/actions/get_log_content.php
+++ php/actions/get_log_content.php
@@ -14,8 +14,21 @@ $log_file = dirname(__FILE__) . "/../../data/logs/$log_file.log";
 
 if (file_exists($log_file)) {
     if ($data = file($log_file)) {
+        $split_value = '-- DONE --';
         // последние 3000 строк
-        $data = array_reverse(array_slice($data, -3000));
+        $data = array_slice($data, -3000);
+        $key = 0;
+        $temp = [];
+        // режем вывод по ключевому слову
+        foreach ($data as $row) {
+            $temp[$key][] = $row;
+            if (strpos($row, $split_value) !== false) {
+                $temp[$key][] = '';
+                $key++;
+            }
+        }
+        // переворачиваем порядок процессов. Последний - вверху.
+        $data = array_merge(...array_reverse($temp));
         $data = implode('<br />', $data) . '<br />';
         echo $data;
     }
