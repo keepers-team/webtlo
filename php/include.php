a82b4a8babf40f4993ccac721b6e9b584b5b4870
NeBioNik <nebionik@users.noreply.github.com>
2023-08-21 00:28:35 +0300

    Back. Implement new Modules and DTO`s

---
+++ php/include.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+include_once dirname(__FILE__) . '/Module/Forums.php';
+include_once dirname(__FILE__) . '/Module/Topics.php';
+include_once dirname(__FILE__) . '/Module/Torrents.php';
+include_once dirname(__FILE__) . '/Module/TopicDetails.php';
+include_once dirname(__FILE__) . '/Module/ReportCreator.php';
+include_once dirname(__FILE__) . '/Module/CloneTable.php';
+include_once dirname(__FILE__) . '/DTO/ForumObject.php';
+include_once dirname(__FILE__) . '/DTO/KeysObject.php';
+include_once dirname(__FILE__) . '/DTO/UpdateDetailsResultObject.php';
