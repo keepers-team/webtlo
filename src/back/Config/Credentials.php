ef01867402ebbebf107b187c060d96617c8f6c2d
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2023-10-02 11:51:30 +0300

    Config. Add user Credentials

---
+++ src/back/Config/Credentials.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KeepersTeam\Webtlo\Config;
+
+/** Данные пользователя для доступа к форуму. */
+final class Credentials
+{
+    public function __construct(
+        public readonly string $userName,
+        public readonly string $password,
+        public readonly int    $userId,
+        public readonly string $btKey,
+        public readonly string $apiKey,
+    ) {
+    }
+}
