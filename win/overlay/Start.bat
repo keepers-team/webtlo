9df072faab6aa587d44ad028fe9a8843627ad074
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-02-28 00:50:58 +0300

    Standalone. Add openssl cert

--- win/overlay/Start.bat
+++ win/overlay/Start.bat
@@ -1,5 +1,8 @@
+utils\iniEdit.exe "php/php.ini" [SSL] STANDALONE_DIR=%cd%
+
 cd nginx
 start nginx.exe
+
 cd ..\php
 start RunHiddenConsole.exe php-cgi.exe -b 127.0.0.1:39081
 start "" http://localhost:39080/
