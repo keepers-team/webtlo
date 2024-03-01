f870990e2e14b03a750ae2f820060a6bd1024532
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-03-01 19:08:10 +0300

    Update get_recovery_list

--- src/php/actions/get_recovery_list.php
+++ src/php/actions/get_recovery_list.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 use KeepersTeam\Webtlo\App;
@@ -9,40 +11,80 @@ use KeepersTeam\Webtlo\Legacy\Log;
 try {
     $cfg = App::getSettings();
 
-    $output = [];
+    $counters = Db::query_database(
+        'SELECT tu.status, COUNT(1) AS quantity
+        FROM TopicsUnregistered AS tu
+            LEFT JOIN Torrents AS tr ON tr.info_hash = tu.info_hash
+        WHERE tr.done = 1.0
+            AND tu.priority IS NOT ("низкий")
+        GROUP BY tu.status
+        ORDER BY 2 DESC',
+        [],
+        true
+    );
+
+    if (count($counters)) {
+        $table = '';
+        foreach ($counters as $row) {
+            $table.= vsprintf("<tr><td>%s</td><td>%s</td></tr>", $row);
+        }
+        echo "<table><thead><th>Статус</th><th>Количество</th></thead>$table</table></br>";
+    }
 
     $allowedStatuses = [
         'проверено',
         'не проверено',
         'недооформлено',
-        // 'временная',
-        'сомнительно'
+        'временная',
+        'сомнительно',
     ];
+
+    if (!empty($_GET['statuses'])) {
+        $allowedStatuses = explode(',', $_GET['statuses']);
+    }
 
     $in = str_repeat('?,', count($allowedStatuses) - 1) . '?';
 
     $topics = Db::query_database(
         'SELECT
-            Torrents.topic_id,
-            status,
-            transferred_from,
-            transferred_to,
-            transferred_by_whom
-        FROM TopicsUnregistered
-        LEFT JOIN Torrents ON Torrents.info_hash = TopicsUnregistered.info_hash
-        WHERE
-            priority IS NOT "низкий"
-            AND Torrents.done = 1.0
-            AND status IN (' . $in . ')',
+            tr.topic_id,
+            tu.status,
+            tu.transferred_from,
+            tu.transferred_to,
+            tu.transferred_by_whom
+        FROM TopicsUnregistered AS tu
+        LEFT JOIN Torrents AS tr ON tr.info_hash = tu.info_hash
+        WHERE tr.done = 1.0
+            AND tu.priority IS NOT ("низкий")
+            AND tu.status IN (' . $in . ')',
         $allowedStatuses,
         true
     );
 
+    if (!count($topics)) {
+        echo 'Нет разрегистрированных раздач доступных к восстановлению.';
+
+        return;
+    }
+
+
+    $output = [];
     foreach ($topics as $topic) {
-        $currentCategory = explode(' » ', $topic['transferred_to']);
         $categoryTitle = empty($topic['transferred_from']) ? $topic['transferred_to'] : $topic['transferred_from'];
-        $output[$currentCategory[0]][] = $categoryTitle  . ' | [url=viewtopic.php?t=' . $topic['topic_id'] . ']' .
-            $topic['topic_id'] . '[/url] | ' . $topic['status'] . ' | ' . $topic['transferred_by_whom'];
+
+        $currentCategory = explode(' » ', $topic['transferred_to']);
+        $currentCategory = $currentCategory[0];
+
+        $output[$currentCategory][] = sprintf(
+            "%s | [url=viewtopic.php?t=%d]%d[/url] | %s | %s",
+            $categoryTitle,
+            (int)$topic['topic_id'],
+            (int)$topic['topic_id'],
+            $topic['status'],
+            $topic['transferred_by_whom']
+        );
+
+        unset($topic);
     }
     unset($topics);
 
@@ -50,6 +92,7 @@ try {
 
     foreach ($output as $categoryName => $categoryData) {
         asort($categoryData, SORT_NATURAL);
+
         echo '[b]' . $categoryName . '[/b][hr]</br>';
         foreach ($categoryData as $topicData) {
             echo $topicData . '</br>';
