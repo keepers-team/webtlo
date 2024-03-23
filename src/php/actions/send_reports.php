7132cce18f6299d01d184303867c081300dbf57b
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-03-23 17:54:10 +0300

    Refactor send_report action

--- src/php/actions/send_reports.php
+++ src/php/actions/send_reports.php
@@ -1,20 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use KeepersTeam\Webtlo\AppContainer;
 use KeepersTeam\Webtlo\Legacy\Log;
 
 $reports_result = [
     'result' => '',
 ];
+
+// Создаём контейнер и пишем в лог.
+$app = AppContainer::create('reports.log');
+$log = $app->getLogger();
+
 try {
     // дёргаем скрипт
-    // include_once dirname(__FILE__) . '/../common/reports.php';
-    include_once dirname(__FILE__) . '/../common/reports_via_api.php';
-} catch (Exception $e) {
-    Log::append($e->getMessage());
+    include_once dirname(__FILE__) . '/../common/reports.php';
+
+    $log->info('-- DONE --');
+} catch (Throwable $e) {
+    $log->error($e->getMessage());
+
     $reports_result['result'] = 'В процессе отправки отчётов были ошибки. ' .
         'Для получения подробностей обратитесь к журналу событий.';
 }
-// выводим лог
+
+// Выводим лог.
 $reports_result['log'] = Log::get();
 
 echo json_encode($reports_result, JSON_UNESCAPED_UNICODE);
