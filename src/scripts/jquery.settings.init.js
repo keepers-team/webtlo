b6918dfbac02e2ad73178e2816ad7bf50aae7153
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-06-18 21:48:21 +0300

    Add profile and subforum links
    - Update forum profile link functionality and add subforum link opening capability. 
    - Add new script file for settings functions.

--- src/scripts/jquery.settings.init.js
+++ src/scripts/jquery.settings.init.js
@@ -11,6 +11,23 @@ $(document).ready(function () {
         } else {
             elems.prop('type', 'text');
         }
-    })
+    });
+
+    // Открыть ссылку на профиль пользователя.
+    $('#tracker_username').next('i').click(function(e) {
+        e.preventDefault();
+
+        let user = $('#user_id').val();
+        if (!user) {
+            user = $('#tracker_username').val();
+        }
+        if (!user) {
+            return;
+        }
+
+        const domain = getForumUrl()
+        const url = `${domain}/forum/profile.php?mode=viewprofile&u=${user}`;
+        window.open(url, '_blank');
+    });
 
 });
