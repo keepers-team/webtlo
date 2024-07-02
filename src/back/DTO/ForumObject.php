5b17995e12d23661f2a6243a6da712ab0f1967b2
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-07-02 15:37:38 +0300

    Refactor ForumObject constructor parameters
    - Simplified the ForumObject constructor by removing nullable properties.
    - Updated methods to reflect changes in parameter requirements.

--- src/back/DTO/ForumObject.php
+++ src/back/DTO/ForumObject.php
@@ -7,16 +7,10 @@ namespace KeepersTeam\Webtlo\DTO;
 final class ForumObject
 {
     public function __construct(
-        public int     $id,
-        public string  $name,
-        public int     $quantity,
-        public int     $size,
-        public ?int    $topic_id,
-        public ?int    $author_id,
-        public ?string $author_name,
-        public ?int    $author_post_id,
-        /** @var ?int[] */
-        public ?array  $post_ids
+        public int    $id,
+        public string $name,
+        public int    $quantity,
+        public int    $size,
     ) {
     }
 }
