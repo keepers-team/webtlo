48dec365209b17f4f7778b3b6aff2ed4344821e2
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-07-15 06:18:21 +0300

    Refactor AppContainer to include ReportSend configuration
    - Introduce ReportSend class for report settings

---
+++ src/back/Config/ReportSend.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KeepersTeam\Webtlo\Config;
+
+use KeepersTeam\Webtlo\Helper;
+
+/**
+ * Параметры отправки и получения отчётов.
+ */
+final class ReportSend
+{
+    /**
+     * @param bool  $sendReports         Отправлять отчёты по хранимым подразделам.
+     * @param bool  $sendSummary         Отправлять сводный отчёт.
+     * @param bool  $sendTelemetry       Отправлять дополнительные сведения об установке программы и настройках.
+     * @param bool  $unsetOtherTopics    При отправке отчёта по подразделу, снимать признак хранения с раздач, которых больше нет в БД (в т.ч. разрегистрированные и обновлённые раздачи).
+     * @param bool  $unsetOtherSubForums Снимать признак хранения у подразделов, которые более не хранятся, согласно настроек.
+     * @param int   $daysUpdateExpire    Количество дней обновления данных, после истечения которых, отправка отчётов невозможна.
+     * @param int[] $excludedSubForums   Исключённые из отправки отчётов хранимые подразделы.
+     * @param int[] $excludedClients     Исключённые из отправки отчётов торрент-клиенты.
+     * @param int[] $excludedKeepers     Игнорируемые хранители.
+     */
+    public function __construct(
+        public readonly bool  $sendReports,
+        public readonly bool  $sendSummary,
+        public readonly bool  $sendTelemetry,
+        public readonly bool  $unsetOtherTopics,
+        public readonly bool  $unsetOtherSubForums,
+        public readonly int   $daysUpdateExpire,
+        public readonly array $excludedSubForums,
+        public readonly array $excludedClients,
+        public readonly array $excludedKeepers,
+    ) {
+    }
+
+    /**
+     * @param array<string, mixed> $cfg
+     */
+    public static function getReportSend(array $cfg): ReportSend
+    {
+        $report = $cfg['reports'] ?? [];
+
+        return new ReportSend(
+            sendReports        : (bool)($report['send_report_api'] ?? 1),
+            sendSummary        : (bool)($report['send_summary_report'] ?? 1),
+            sendTelemetry      : (bool)($report['send_report_settings'] ?? 1),
+            unsetOtherTopics   : (bool)($report['unset_other_topics'] ?? 0),
+            unsetOtherSubForums: (bool)($report['unset_other_forums'] ?? 1),
+            daysUpdateExpire   : (int)($report['days_update_expire'] ?? 5),
+            excludedSubForums  : Helper::explodeInt((string)($report['exclude_forums_ids'] ?? '')),
+            excludedClients    : Helper::explodeInt((string)($report['exclude_clients_ids'] ?? '')),
+            excludedKeepers    : Helper::explodeInt((string)($report['exclude_keepers_ids'] ?? '')),
+        );
+    }
+}
