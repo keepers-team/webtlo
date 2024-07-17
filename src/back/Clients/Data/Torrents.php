4abb202e8aa1fcc99d659239faaeabeffbdb77f1
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-07-17 07:27:39 +0300

    Add Torrent and Torrents classes for managing torrent client data
    - Introduce Torrent class to represent individual torrents
    - Create Torrents class to handle collections of torrents
    - Implement Countable interface for counting torrents

---
+++ src/back/Clients/Data/Torrents.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KeepersTeam\Webtlo\Clients\Data;
+
+use Countable;
+use Generator;
+
+/**
+ * Набор раздач из одного торрент-клиента.
+ */
+final class Torrents implements Countable
+{
+    /**
+     * @param array<string, Torrent> $torrents
+     */
+    public function __construct(public readonly array $torrents = [])
+    {
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getHashes(): array
+    {
+        return array_map('strval', array_keys($this->torrents));
+    }
+
+    public function getTorrent(string $hash): ?Torrent
+    {
+        return $this->torrents[strtoupper($hash)] ?? null;
+    }
+
+    public function getGenerator(): Generator
+    {
+        foreach ($this->torrents as $hash => $torrent) {
+            yield $hash => $torrent;
+        }
+    }
+
+    // Countable
+
+    public function count(): int
+    {
+        return count($this->torrents);
+    }
+
+    public function empty(): bool
+    {
+        return $this->torrents === [];
+    }
+}
