72ba0f590ded1f2d04f94866f4a38c1a963a30a2
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-08-21 22:07:28 +0300

    Add new TopicControl options
    - Add parameters for days until unseeded and max unseeded count
    - Add parameter for randomApplyCount
    - Update Settings class to read and write new TopicControl settings

--- src/back/Config/TopicControl.php
+++ src/back/Config/TopicControl.php
@@ -17,19 +17,27 @@ final class TopicControl
      * Сиды-хранители ⊆ Сиды
      */
 
+    public const UnknownHashes = 'UnknownHashes';
+
     /**
      * @param int  $peersLimit             Предел пиров при регулировке
      * @param int  $excludedKeepersCount   Количество исключаемых из регулировки хранителей на раздаче
+     * @param int  $randomApplyCount       Разница пиров, при которой применяется рандом переключения состояния раздачи
      * @param bool $countLeechersAsPeers   Учитывать личей при подсчёте пиров
      * @param bool $seedingWithoutLeechers Сидировать раздачи, на которых нет личей
      * @param bool $manageOtherSubsections Регулировать раздачи из прочих подразделов
+     * @param int  $daysUntilUnseeded      Количество дней, по прошествии которых раздача считается не сидируемой
+     * @param int  $maxUnseededCount       Максимальное количество не сидируемых раздач, которые можно запустить одновременно
      */
     public function __construct(
         public readonly int  $peersLimit,
         public readonly int  $excludedKeepersCount,
+        public readonly int  $randomApplyCount,
         public readonly bool $countLeechersAsPeers,
         public readonly bool $seedingWithoutLeechers,
         public readonly bool $manageOtherSubsections,
+        public readonly int  $daysUntilUnseeded,
+        public readonly int  $maxUnseededCount,
     ) {
     }
 
@@ -43,9 +51,12 @@ final class TopicControl
         return new TopicControl(
             peersLimit            : (int)($control['peers'] ?? 10),
             excludedKeepersCount  : (int)($control['keepers'] ?? 3),
+            randomApplyCount      : (int)($control['random'] ?? 1),
             countLeechersAsPeers  : (bool)($control['leechers'] ?? 0),
             seedingWithoutLeechers: (bool)($control['no_leechers'] ?? 1),
             manageOtherSubsections: (bool)($control['unadded_subsections'] ?? 0),
+            daysUntilUnseeded     : (int)($control['days_until_unseeded'] ?? 21),
+            maxUnseededCount      : (int)($control['max_unseeded_count'] ?? 100),
         );
     }
 }
