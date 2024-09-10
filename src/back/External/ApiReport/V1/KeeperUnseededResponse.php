68e68590e39f5fcb9916fe0d77393d6409317c80
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-09-10 18:03:44 +0300

    Add KeeperUnseededTopics trait for retrieving unseeded topics
    - Introduces a new trait `KeeperUnseededTopics` to handle the retrieval of unseeded topics in a subforum
    - Includes a new class `KeeperUnseededResponse` with methods to filter out hashes based on seeding days

---
+++ src/back/External/ApiReport/V1/KeeperUnseededResponse.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KeepersTeam\Webtlo\External\ApiReport\V1;
+
+use DateTimeImmutable;
+use Throwable;
+
+final class KeeperUnseededResponse
+{
+    /**
+     * @param int                 $subForumId Ид подраздела
+     * @param int                 $totalCount Количество хранимых раздач в подразделе
+     * @param string[]            $columns
+     * @param array<int, mixed>[] $releases
+     * @param DateTimeImmutable   $cacheTime  Дата хеширования ответа
+     */
+    public function __construct(
+        public readonly int               $subForumId,
+        public readonly int               $totalCount,
+        public readonly DateTimeImmutable $cacheTime,
+        private readonly array            $columns,
+        private readonly array            $releases,
+    ) {
+    }
+
+    /**
+     * Найти хеши раздач, которые не сидировались более заданного количества дней.
+     *
+     * @return string[]
+     */
+    public function getHashes(int $notSeedingDays): array
+    {
+        $cutoffDate = self::calculateCutoffDate($notSeedingDays);
+
+        $hashes = [];
+        foreach ($this->releases as $release) {
+            $topic = array_combine($this->columns, $release);
+
+            // Если даты нет, значит её очень давно не сидировали, добавляем в список.
+            if (null === $topic['last_seeded_time']) {
+                $hashes[] = $topic['info_hash'];
+                continue;
+            }
+
+            try {
+                $lastSeeded = new DateTimeImmutable($topic['last_seeded_time']);
+            } catch (Throwable) {
+                // Пропускаем некорректные даты.
+                continue;
+            }
+
+            // Если дата последнего сидирования меньше(старше) даты отсечки, добавляем в список.
+            if ($lastSeeded < $cutoffDate) {
+                $hashes[] = $topic['info_hash'];
+            }
+        }
+
+        return array_map('strval', $hashes);
+    }
+
+    /**
+     * Рассчитывает дату отсечки на основе количества дней.
+     *
+     * @param int $days Количество дней для отсечки.
+     * @return DateTimeImmutable Дата отсечки.
+     */
+    private static function calculateCutoffDate(int $days): DateTimeImmutable
+    {
+        return (new DateTimeImmutable("-$days days"))->setTime(0, 0);
+    }
+}
