c2d49ac10312644e99495e4f8589dae114879fb9
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-10-25 00:55:45 +0300

    [php-cs] Use @PhpCsFixer ruleset
    - no_useless_else
    - no_useless_return
    - no_unused_imports
    - ordered_traits
    - ordered_imports\
    - global_namespace_import
    
    
    
    - Update custom rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -5,20 +5,40 @@ $finder = PhpCsFixer\Finder::create()
 ;
 
 $config = new PhpCsFixer\Config();
-$rules  = [
-    '@PER-CS'         => true,
-    '@PHP83Migration' => true,
 
-    'no_empty_statement'     => true,
-    'standardize_not_equals' => true,
+$rules = [
+    '@PER-CS'     => true,
+    '@PhpCsFixer' => true,
 
-    'function_declaration' => [
+    'global_namespace_import' => ['import_classes' => true],
+    'function_declaration'    => [
         'closure_fn_spacing'       => 'none',
         'closure_function_spacing' => 'none',
     ],
+
+    'phpdoc_to_comment' => false,
+
+    'single_line_comment_style' => ['comment_types' => ['hash']],
+
+    'ordered_types'      => ['null_adjustment' => 'always_first', 'sort_algorithm' => 'none'],
+    'phpdoc_types_order' => ['null_adjustment' => 'always_first', 'sort_algorithm' => 'none'],
+
+    'ordered_class_elements'   => false,
+    'explicit_string_variable' => false,
+
+    'blank_line_before_statement' => true,
+    'type_declaration_spaces'     => false,
+
+    'binary_operator_spaces' => [
+        'default'   => 'at_least_single_space',
+        'operators' => ['=' => 'align'],
+    ],
+
+    'concat_space' => ['spacing' => 'one'],
 ];
 
 return $config
     ->setRules($rules)->setFinder($finder)
+    ->setRiskyAllowed(true)
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
 ;
