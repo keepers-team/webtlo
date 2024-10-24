3f0c9a0201722459fbafc95a544ee837d13df48f
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2024-10-24 12:31:11 +0300

    Add CloneTable object and module

---
+++ src/back/DTO/TableCloneObject.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KeepersTeam\Webtlo\DTO;
+
+final class TableCloneObject
+{
+    /**
+     * @param string           $origin  Название оригинальной таблицы.
+     * @param string           $clone   Название временной таблицы хранения данных.
+     * @param array{}|string[] $keys    Поля/ключи таблицы.
+     * @param string           $primary PRIMARY KEY таблицы.
+     */
+    public function __construct(
+        public readonly string $origin,
+        public readonly string $clone,
+        public readonly array  $keys,
+        public readonly string $primary
+    ) {
+    }
+
+    public function getKeysSelect(): string
+    {
+        if (count($this->keys)) {
+            return implode(',', $this->keys);
+        }
+
+        return '*';
+    }
+
+    public function getKeysInsert(): string
+    {
+        if (count($this->keys)) {
+            return sprintf('(%s)', implode(',', $this->keys));
+        }
+
+        return '';
+    }
+}
