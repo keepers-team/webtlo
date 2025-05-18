08fe5d432f36e213a41b04566db7489a1d3b2d16
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2025-05-18 18:36:36 +0300

    ApiReport. Update get KeepersResponse
    - Add seeding flag
    - Add topicsCount
    - Use Generator instead of raw array

--- src/back/External/ApiReport/V1/KeeperTopics.php
+++ src/back/External/ApiReport/V1/KeeperTopics.php
@@ -8,6 +8,7 @@ final class KeeperTopics
 {
     public function __construct(
         public readonly int   $keeperId,
+        public readonly int   $topicsCount,
         /** @var KeptTopic[] */
         public readonly array $topics
     ) {}
