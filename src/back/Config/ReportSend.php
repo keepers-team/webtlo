599f29bd294786e2231b3e80150037b5ca64a6bd
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2025-05-22 23:20:36 +0300

    Config. Remove unused construct methods

--- src/back/Config/ReportSend.php
+++ src/back/Config/ReportSend.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace KeepersTeam\Webtlo\Config;
 
-use KeepersTeam\Webtlo\Helper;
-
 /**
  * Параметры отправки и получения отчётов.
  */
@@ -15,7 +13,7 @@ final class ReportSend
      * @param bool  $sendReports         отправлять отчёты по хранимым подразделам
      * @param bool  $sendSummary         отправлять сводный отчёт
      * @param bool  $sendTelemetry       отправлять дополнительные сведения об установке программы и настройках
-     * @param bool  $unsetOtherTopics    При отправке отчёта по подразделу, снимать признак хранения с раздач, которых больше нет в БД (в т.ч. разрегистрированные и обновлённые раздачи).
+     * @param bool  $unsetOtherTopics    при отправке отчёта по подразделу, снимать признак хранения с раздач, которых больше нет в БД (в т.ч. разрегистрированные и обновлённые раздачи)
      * @param bool  $unsetOtherSubForums снимать признак хранения у подразделов, которые более не хранятся, согласно настроек
      * @param int   $daysUpdateExpire    количество дней обновления данных, после истечения которых, отправка отчётов невозможна
      * @param int[] $excludedSubForums   исключённые из отправки отчётов хранимые подразделы
@@ -34,25 +32,4 @@ final class ReportSend
         public readonly array $excludedClients,
         public readonly array $excludedKeepers,
     ) {}
-
-    /**
-     * @param array<string, mixed> $cfg
-     */
-    public static function getReportSend(array $cfg): ReportSend
-    {
-        $report = $cfg['reports'] ?? [];
-
-        return new ReportSend(
-            sendReports        : (bool) ($report['send_report_api'] ?? 1),
-            sendSummary        : (bool) ($report['send_summary_report'] ?? 1),
-            sendTelemetry      : (bool) ($report['send_report_settings'] ?? 1),
-            excludeAuthored    : (bool) ($report['exclude_authored'] ?? 0),
-            unsetOtherTopics   : (bool) ($report['unset_other_topics'] ?? 0),
-            unsetOtherSubForums: (bool) ($report['unset_other_forums'] ?? 1),
-            daysUpdateExpire   : (int) ($report['days_update_expire'] ?? 5),
-            excludedSubForums  : Helper::explodeInt((string) ($report['exclude_forums_ids'] ?? '')),
-            excludedClients    : Helper::explodeInt((string) ($report['exclude_clients_ids'] ?? '')),
-            excludedKeepers    : Helper::explodeInt((string) ($report['exclude_keepers_ids'] ?? '')),
-        );
-    }
 }
--- src/back/Config/TopicControl.php
+++ src/back/Config/TopicControl.php
@@ -44,25 +44,4 @@ final class TopicControl
         public readonly int      $daysUntilUnseeded,
         public readonly int      $maxUnseededCount,
     ) {}
-
-    /**
-     * @param array<string, mixed> $cfg
-     */
-    public static function getTopicControl(array $cfg): TopicControl
-    {
-        $control = $cfg['topics_control'] ?? [];
-
-        return new TopicControl(
-            peersLimit            : (int) ($control['peers'] ?? 10),
-            peersLimitIntervals   : (string) ($control['intervals'] ?? ''),
-            excludedKeepersCount  : (int) ($control['keepers'] ?? 3),
-            randomApplyCount      : (int) ($control['random'] ?? 1),
-            peerLimitPriority     : Priority::from((int) ($control['priority'] ?? 1)),
-            countLeechersAsPeers  : (bool) ($control['leechers'] ?? 0),
-            seedingWithoutLeechers: (bool) ($control['no_leechers'] ?? 1),
-            manageOtherSubsections: (bool) ($control['unadded_subsections'] ?? 0),
-            daysUntilUnseeded     : (int) ($control['days_until_unseeded'] ?? 21),
-            maxUnseededCount      : (int) ($control['max_unseeded_count'] ?? 100),
-        );
-    }
 }
