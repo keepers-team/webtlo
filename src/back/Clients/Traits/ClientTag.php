87efed00cbf97bec8d0ad7015c0d941037b51776
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2025-05-31 21:15:04 +0300

    Refactor TorrentClients usage

--- src/back/Clients/Traits/ClientTag.php
+++ src/back/Clients/Traits/ClientTag.php
@@ -8,19 +8,6 @@ trait ClientTag
 {
     public function getClientTag(): string
     {
-        $tag = $this->options->type->value;
-
-        $extra = $this->options->extra;
-        if (count($extra) > 0) {
-            if (!empty($extra['comment'])) {
-                $tag = $extra['comment'];
-            }
-
-            if (!empty($extra['id'])) {
-                $tag .= sprintf('(%s)', $extra['id']);
-            }
-        }
-
-        return $tag;
+        return $this->options->tag;
     }
 }
