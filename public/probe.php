1daa0028a0f9c092d496fd8a334ccfef6551a8e1
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2026-02-16 12:55:29 +0300

    Fix index + probe vendor path

--- public/probe.php
+++ public/probe.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use KeepersTeam\Webtlo\App;
 use KeepersTeam\Webtlo\Module\ProbeChecker;
