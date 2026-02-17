8ed5202ee920c105e0f80cf3319fe1bc4e4b84c4
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2026-02-18 01:25:34 +0300

    Cron. Move deprecated cron actions

---
+++ cron/keepers.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use KeepersTeam\Webtlo\Console\ConsoleKernel;
+
+/**
+ * Запуск обновления списка хранителей строго из планировщика.
+ *
+ * На возможность выполнения влияет опция "Автоматизация и дополнительные настройки" > "[update.php, keepers.php]".
+ *
+ * @deprecated Use bin/webtlo instead
+ *
+ * @TODO       remove in 4.2
+ */
+$kernel = new ConsoleKernel();
+
+exit($kernel->handle([__FILE__, 'cron:keepers']));
