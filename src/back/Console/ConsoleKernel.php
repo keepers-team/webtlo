4e0747bab76d68cc26aa24f5e2ad7eb5b9059bfc
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2026-02-18 18:43:02 +0300

    Cron. Add lock strategy

--- src/back/Console/ConsoleKernel.php
+++ src/back/Console/ConsoleKernel.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace KeepersTeam\Webtlo\Console;
 
 use KeepersTeam\Webtlo\App;
+use KeepersTeam\Webtlo\Helper;
 use RuntimeException;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\Store\FlockStore;
 use Throwable;
 
 final class ConsoleKernel
@@ -42,16 +45,29 @@ final class ConsoleKernel
             echo $notice . PHP_EOL;
         }
 
+        $automation = $app->getAutomation();
+
+        if (!$automation->isCommandEnabled(command: $command)) {
+            $message = sprintf('[%s] Автоматическое выполнение отключено в настройках.', $command->name);
+
+            $logger->notice($message);
+            echo $message . PHP_EOL;
+
+            return 0;
+        }
+
+        $store   = new FlockStore(Helper::getStorageDir() . '/locks');
+        $factory = new LockFactory($store);
+
+        $lock = $factory->createLock('webtlo-global', 1800);
+
         try {
-            $automation = $app->getAutomation();
+            if (!$lock->acquire()) {
+                $message = 'Console. Запуск невозможен. Запущен другой процесс.';
+                $logger->warning($message);
+                echo "$message\n";
 
-            if (!$automation->isCommandEnabled(command: $command)) {
-                $message = sprintf('[%s] Автоматическое выполнение отключено в настройках.', $command->name);
-
-                $logger->notice($message);
-                echo $message . PHP_EOL;
-
-                exit(0);
+                return 1;
             }
 
             $command->run(app: $app);
@@ -68,6 +84,7 @@ final class ConsoleKernel
 
             return 1;
         } finally {
+            $lock->release();
             $logger->info('-- DONE --');
         }
     }
