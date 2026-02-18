011dc893ef4f78962628e130d03a27b206b55782
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2026-02-18 10:00:54 +0300

    Build. Fix win-build paths

--- win/overlay/schedule-install.bat
+++ win/overlay/schedule-install.bat
@@ -7,9 +7,9 @@ SCHTASKS /DELETE /TN "WebTLO\Keepers" /F
 SCHTASKS /DELETE /TN "WebTLO\Reports" /F
 
 echo Adding tasks...
-SCHTASKS /CREATE /SC HOURLY /TN "WebTLO\Control" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\cron\control.php'" /ST 00:25
-SCHTASKS /CREATE /SC HOURLY /TN "WebTLO\Update"  /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\cron\update.php'"  /ST 00:15
-SCHTASKS /CREATE /SC DAILY  /TN "WebTLO\Keepers" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\cron\keepers.php'" /ST 05:00
-SCHTASKS /CREATE /SC DAILY  /TN "WebTLO\Reports" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\cron\reports.php'" /ST 06:00
+SCHTASKS /CREATE /SC HOURLY /TN "WebTLO\Control" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\bin\webtlo' 'cron:control'" /ST 00:25
+SCHTASKS /CREATE /SC HOURLY /TN "WebTLO\Update"  /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\bin\webtlo' 'cron:update'"  /ST 00:15
+SCHTASKS /CREATE /SC DAILY  /TN "WebTLO\Keepers" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\bin\webtlo' 'cron:keepers'" /ST 05:00
+SCHTASKS /CREATE /SC DAILY  /TN "WebTLO\Reports" /TR "'%cd%\php\RunHiddenConsole.exe' '%cd%\php\php.exe' '%cd%\nginx\wtlo\bin\webtlo' 'cron:reports'" /ST 06:00
 
 pause
