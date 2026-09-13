1af5cfb49fe7f13da4ff3cb4da82432438b9f6d8
NeBioNik <54838254+NeBioNik@users.noreply.github.com>
2026-09-13 16:48:41 +0300

    Report. Add new send by hashes option

--- src/back/External/ApiReportClient.php
+++ src/back/External/ApiReportClient.php
@@ -87,7 +87,7 @@ final class ApiReportClient
         array             $topicIds,
         int               $status,
         DateTimeInterface $reportDate,
-        bool              $excludeOtherReleases = false,
+        bool              $excludeOther = false,
     ): ?array {
         $params = [
             'keeper_id'                           => $this->auth->userId,
@@ -95,11 +95,57 @@ final class ApiReportClient
             'status'                              => $status,
             'last_update_time'                    => $reportDate->format(DateTimeInterface::ATOM),
             'reported_subforum_id'                => $forumId,
-            'unreport_other_releases_in_subforum' => $excludeOtherReleases,
+            'unreport_other_releases_in_subforum' => $excludeOther,
         ];
 
         try {
             $response = $this->client->post('releases/set_status', ['json' => $params]);
+        } catch (GuzzleException $e) {
+            $this->logException($e->getCode(), $e->getMessage(), $params);
+
+            return null;
+        }
+
+        $body = $response->getBody()->getContents();
+
+        return json_decode($body, true);
+    }
+
+    /**
+     * Просто отправить список известных хешей со статусом хранения.
+     *
+     * @param string[] $topicHashes
+     *
+     * @return ?array<string, mixed>
+     */
+    public function reportKeptReleasesHashes(
+        array             $topicHashes,
+        int               $status,
+        DateTimeInterface $reportDate,
+        bool              $excludeOther = false,
+    ): ?array {
+        $params = [
+            'keeper_id'        => $this->auth->userId,
+            'topic_hashes'     => $topicHashes,
+            'status'           => $status,
+            'last_update_time' => $reportDate->format(DateTimeInterface::ATOM),
+        ];
+
+        // По умолчанию - 60 дней, в соответствии с регламентом.
+        $params['unreport_older_than'] = 'P60D';
+
+        /**
+         * Если указано снять отметку хранения с раздач, которые явно не переданы,
+         * то отправляем интервал 2 мин, чтобы все части отчёта успели дойти.
+         *
+         * Раздачи, старше этого периода будут исключены из хранимых.
+         */
+        if ($excludeOther) {
+            $params['unreport_older_than'] = 'PT2M';
+        }
+
+        try {
+            $response = $this->client->post('releases/set_status_by_hash', ['json' => $params]);
         } catch (GuzzleException $e) {
             $this->logException($e->getCode(), $e->getMessage(), $params);
 
@@ -197,6 +243,34 @@ final class ApiReportClient
         $body = json_decode($response->getBody()->getContents(), true);
 
         return $body ?: ['result' => 'unknown'];
+    }
+
+    /**
+     * Триггер для API отчётов, для автоматического определения статуса хранимых подразделов
+     * на основании переданных хешей хранимых раздач.
+     *
+     * @return array<string, mixed>
+     */
+    public function setForumsStatusAuto(): array
+    {
+        $params = [
+            'keeper_id'           => $this->auth->userId,
+            'ignore_non_reported' => true, // Указание API не учитывать раздачи "по сидированию"
+        ];
+
+        try {
+            // POST запрос с GET параметрами.
+            $response = $this->client->post('subforum/set_status_auto', ['query' => $params]);
+        } catch (GuzzleException $e) {
+            $this->logException($e->getCode(), $e->getMessage(), $params);
+
+            return ['result' => $e->getMessage()];
+        }
+
+        $body = json_decode($response->getBody()->getContents(), true) ?: ['result' => 'unknown'];
+        unset($body['dry_run'], $body['details']);
+
+        return $body;
     }
 
     /**
